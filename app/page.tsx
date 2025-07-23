186d2451270111bf2056c8cb271cebb385f9983c
SAM50x <149119430+Sam50x@users.noreply.github.com>
2025-07-23 13:16:08 +0300

    styling variables

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
 
   return (
-    <main className="flex justify-center items-center flex-col pb-12">
-      
+    <main className="">
+          <h1 className="">Font bg</h1>
     </main>
   );
 }

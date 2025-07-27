7ae87c554bdcc7a8f979561c535787f916a4336b
SAM50x <149119430+Sam50x@users.noreply.github.com>
2025-07-27 14:46:31 +0300

    Update cloudinary.ts

--- app/lib/cloudinary.ts
+++ app/lib/cloudinary.ts
@@ -5,9 +5,9 @@ import { v2 as cloudinary } from 'cloudinary'
 const { NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME, NEXT_PUBLIC_CLOUDINARY_API_KEY, NEXT_PUBLIC_CLOUDINARY_API_SECRET } = process.env
 
 cloudinary.config({
-    cloud_name: NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-    api_key: NEXT_PUBLIC_CLOUDINARY_API_KEY,
-    api_secret: NEXT_PUBLIC_CLOUDINARY_API_SECRET,
+    cloud_name: NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME!,
+    api_key: NEXT_PUBLIC_CLOUDINARY_API_KEY!,
+    api_secret: NEXT_PUBLIC_CLOUDINARY_API_SECRET!,
 });
 
 export default cloudinary
